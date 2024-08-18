22f9e4b46895226af3bad724f07cf8c539bd29a9
David Schuler <dschuler36@gmail.com>
2024-08-18 10:44:55 -0400

    Feat/UI (#16)
    * initial ui page
    
    * updating page title

---
+++ ui/src/routes/+page.server.ts
@@ -0,0 +1,30 @@
+import type { Load } from '@sveltejs/kit';
+
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+async function getHalfPprPredictions(season: number, week: number) {
+    const response = await fetch(`${API_BASE_URL}/api/predictions/half_ppr?season=${season}&week=${week}`);
+    const responseData = await response.json();
+    return responseData;
+}
+
+export const load: Load = async ({ parent }) => {
+    try {
+        const halfPprPredictions = await getHalfPprPredictions(2023, 1);
+        return {
+            props: {
+                halfPprPredictions
+            }
+        };
+        } 
+     catch (error) {
+        console.error('Error fetching leagues:', error);
+        return {
+            status: 500,
+            error: {
+                message: 'Internal Server Error',
+                details: error.message
+            }
+        };
+    }
+};
