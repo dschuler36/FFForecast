b9852bfaaa78a9e6b69a233538bc2d5894541eca
dschuler36 <dschuler36@gmail.com>
2024-08-18 21:06:01 -0400

    enabled toggling between half / full ppr

--- api/src/api/services/predictions_service.py
+++ api/src/api/services/predictions_service.py
@@ -12,7 +12,7 @@ class PredictionsService:
     def get_base_predictions_query(season: int, week: int, league_config_predictions):
         stmt = (
             select(WeeklyPredictionBase, league_config_predictions.fantasy_points)
-            .join(WeeklyPredictionStdHalfPPR, WeeklyPredictionBase.player_id == league_config_predictions.player_id)
+            .join(league_config_predictions, WeeklyPredictionBase.player_id == league_config_predictions.player_id)
             .where(
                 WeeklyPredictionBase.season == season,
                 WeeklyPredictionBase.week == week
@@ -32,8 +32,14 @@ class PredictionsService:
             for base, std_half_ppr in predictions
         ]
         return unpacked_predictions
+
     async def get_full_ppr_predictions(self, season: int, week: int):
         stmt = self.get_base_predictions_query(season, week, WeeklyPredictionStdFullPPR)
         result = await self.db.execute(stmt)
-        predictions = result.fetchall()
-        return predictions
+        predictions = result.fetchall()  # This returns a list of tuples (WeeklyPredictionBase, WeeklyPredictionStdHalfPPR)
+        # Unpack tuples into a list of dicts or any other structure you need
+        unpacked_predictions = [
+            {"base": base, "fantasy_points": std_full_ppr}
+            for base, std_full_ppr in predictions
+        ]
+        return unpacked_predictions
