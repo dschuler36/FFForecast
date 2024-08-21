831e84cd15056620f279ff83d8fa4344553fae56
dschuler36 <dschuler36@gmail.com>
2024-08-20 20:18:12 -0400

    feat: running ui on 0.0.0.0 for railway

--- ui/svelte.config.js
+++ ui/svelte.config.js
@@ -12,7 +12,13 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter()
-	}
+	},
+	vite: {
+        server: {
+            host: '0.0.0.0',
+            port: process.env.PORT || 4173,
+        }
+    }
 };
 
 export default config;
